74616303f815456f416bc09ed8fb5dffb1828bc9
Miten09 <124608667+Miten09@users.noreply.github.com>
2023-04-20 19:46:22 +0530

    Create StringMethods.js

---
+++ StringMethods.js
@@ -0,0 +1,50 @@
+// let name = "MITEN";
+
+// console.log(name.length);
+// console.log(name.toUpperCase());
+// console.log(name.toLowerCase());
+// console.log(name.slice(2, 4));
+// console.log(name.slice(2));
+// console.log(name.replace("MIT", "HET"));
+// let friend = "karan";
+
+// console.log(name.concat(" " + friend + " are friends"));
+
+// let otherName = "   karan     ";
+// console.log(otherName);
+// console.log(otherName.trim());
+
+// console.log(name[0]);
+// console.log(name[1]);
+// name[0] = "k"; //not possible because strings are immutable it cannot change
+
+// //practice problems
+
+// const sentence = "the quick brown fox jumped over the lazy dog.";
+// console.log(sentence.includes("dog1"));
+// console.log(sentence.startsWith("brown", 10));
+// console.log(sentence.endsWith("dog"));
+
+// const now = new Date();
+// console.log(now);
+
+// const yr = now.getFullYear();
+// console.log(yr);
+
+// const dt = now.getDate();
+// console.log(dt);
+
+// const month = now.getMonth();
+// console.log(month);
+
+// const hours = now.getHours();
+// console.log(hours);
+
+// const minutes = now.getMinutes();  
+// console.log(minutes);
+
+// now.setDate(1);
+// console.log(now);
+
+// console.log(Date.now)
+
